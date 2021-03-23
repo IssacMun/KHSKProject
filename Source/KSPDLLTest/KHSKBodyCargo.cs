e28aff767c7c212df31101c9361d99744f2a161a
superdog <superlancer@yeah.net>
2021-03-23 22:19:09 +0800

    folder structure changed

---
+++ Source/KSPDLLTest/KHSKBodyCargo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace KHSKController
+{
+    class KHSKBodyCargo : ModuleInventoryPart
+    {
+        public override void OnAwake()
+        {
+            InventorySlots = 6;
+            packedVolumeLimit = 120;
+            massLimit = 0.2f;
+        }
+
+    }
+}
